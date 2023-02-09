c6459f990111b2ddd03b468c1aa71643549b8cca
rosassilvio <122334387+rosassilvio@users.noreply.github.com>
2023-02-09 15:20:20 -0300

    delete src

--- src/main/java/com/nocountry/repository/RolRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nocountry.repository;
-
-import com.nocountry.models.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolRepository extends JpaRepository<Rol,Long> {
-}
